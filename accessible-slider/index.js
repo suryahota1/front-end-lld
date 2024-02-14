bc52b3415a83ba51c61981a7a0336cff9b6efeef
SuryaHota <tech.suryakanthota@gmail.com>
2024-02-14 16:41:03 +0530

    accessible slider

--- accessible-slider/index.js
+++ accessible-slider/index.js
@@ -1,13 +1,64 @@
 class CustomSlider extends HTMLElement {
 
+    thumbPosn;
+    thumbValue;
+    minValue;
+    maxValue;
+    startDragX;
+
     constructor () {
         super();
     }
 
     static observedAttributes = [ "mode", "min", "max", "value", "id", "label" ];
 
-    #attachListeners () {
-        
+    #attachListeners ( ele ) {
+        const thumb = ele.querySelector(".slider-thumb");
+        const sliderContainer = ele.querySelector(".slider");
+        console.log(thumb);
+        thumb.addEventListener("keydown", ( event ) => {
+            // console.log("keydown event", event);
+            if ( event.code === "ArrowRight" || event.code === "ArrowUp" ) {
+                console.log("this.thumbValue-----", this.thumbValue, this.maxValue);
+                if ( this.thumbValue < this.maxValue ) {
+                    console.log("here");
+                    this.thumbValue++;
+                }
+            } else if ( event.code === "ArrowLeft" || event.code === "ArrowDown" ) {
+                if ( this.thumbValue > this.minValue ) this.thumbValue--;
+            }
+            this.thumbPosn = (this.thumbValue - this.minValue) / (this.maxValue - this.minValue) * 100;
+            sliderContainer.setAttribute("aria-valuenow", this.thumbValue);
+            thumb.setAttribute("style", `left: ${this.thumbPosn}%`);
+        });
+
+        function drag ( e ) {
+            console.log("on drag e", e, this);
+            const currX = e.pageX;
+
+        }
+
+        thumb.addEventListener("pointerdown", ( e ) => {
+            console.log("pointerdown e", e);
+            e.preventDefault();
+            this.startDragX = e.pageX;
+            window.addEventListener("pointermove", drag);
+            window.addEventListener("touchmove", drag);
+
+            function removeEvents () {
+                window.removeEventListener("pointermove", drag);
+                window.removeEventListener("touchmove", drag);
+            }
+
+            window.addEventListener("pointerup", ( e ) => {
+                e.preventDefault();
+                removeEvents();
+            });
+            window.addEventListener("touchend", ( e ) => {
+                e.preventDefault();
+                removeEvents();
+            });
+        });
     }
 
     connectedCallback () {
@@ -16,6 +67,9 @@ class CustomSlider extends HTMLElement {
         const sliderId = `custom-slider-${this.getAttribute("id")}`;
 
         const orientation = this.getAttribute("mode") ?? "horizontal";
+        const minVal = this.getAttribute("min") ?? 0;
+        const maxVal = this.getAttribute("max") ?? 100;
+        const value = this.getAttribute("value") ?? 0;
 
         const wrapper = document.createElement("div");
         wrapper.classList.add("slider-wrapper");
@@ -32,17 +86,25 @@ class CustomSlider extends HTMLElement {
         const sliderContainer = document.createElement("div");
         sliderContainer.classList.add(...containerClasses);
         sliderContainer.setAttribute("aria-orientation", orientation);
-        sliderContainer.setAttribute("aria-valuemin", this.getAttribute("min"));
-        sliderContainer.setAttribute("aria-valuemax", this.getAttribute("max"));
-        sliderContainer.setAttribute("aria-valuenow", this.getAttribute("now"));
+        sliderContainer.setAttribute("aria-valuemin", minVal);
+        sliderContainer.setAttribute("aria-valuemax", maxVal);
+        sliderContainer.setAttribute("aria-valuenow", value);
+        sliderContainer.setAttribute("aria-labelledby", sliderId);
 
         const sliderThumb = document.createElement("div");
         sliderThumb.classList.add("slider-thumb");
+        sliderThumb.setAttribute("tabindex", 0);
 
         sliderContainer.appendChild(sliderThumb);
 
         wrapper.appendChild(label);
         wrapper.appendChild(sliderContainer);
+
+        this.thumbValue = Number(value);
+        this.minValue = Number(minVal);
+        this.maxValue = Number(maxVal);
+
+        this.thumbPosn = (this.thumbValue - this.minValue) / (this.maxValue - this.minValue) * 100;
 
         const style = document.createElement("style");
         style.textContent = `
@@ -56,6 +118,7 @@ class CustomSlider extends HTMLElement {
                 width: 200px; 
                 height: 7px;
                 background-color: blue;
+                position: relative;
             }
             .slider-wrapper .slider.vertical {
 
@@ -67,6 +130,8 @@ class CustomSlider extends HTMLElement {
                 width: 15px; 
                 height: 10px;
                 background-color: red;
+                position: absolute;
+                left: ${this.thumbPosn}%
             }
             .slider-wrapper .slider.vertical .slider-thumb {
 
@@ -78,6 +143,7 @@ class CustomSlider extends HTMLElement {
 
         shadow.appendChild(style);
         shadow.appendChild(wrapper);
+        this.#attachListeners(this.shadowRoot);
     }
 
     attributeChangedCallback ( name, oldValue, newValue ) {
